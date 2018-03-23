0c5e8af4953d17d83a3d0606e2141ce3610741cd
Sybren van Wayenburg <sybren@vanwayenburg.net>
2018-03-23 16:00:37 +0100

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,22 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongodb://<dbuser>:<dbpassword>@ds052968.mlab.com:52968/udemy-todo-app
+var connectPath, options;
+//Check if we are on Heroku
+if(process.env.PORT){
+ connectPath = "mongodb://udemy:n6K@elp0ovGX@ds052968.mlab.com:52968/udemy-todo-app";
+ options= {
+     auth: {
+         user: 'udemy',
+         password: 'n6K@elp0ovGX'
+     }
+ }
+}else{
+ connectPath = "mongodb://localhost:27017/TodoApp";
+ options = {}
+}
+mongoose.connect(connectPath, options);
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-// module.exports = {
-//   mongoose: mongoose
-// }
-//ook te schrijven als
 module.exports = {mongoose};
