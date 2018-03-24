28c97967248456069b079e09416b70745233ebde
Sybren van Wayenburg <sybren@vanwayenburg.net>
2018-03-24 22:45:15 +0100

    Mlab uri from environment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,24 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-//mongodb://<dbuser>:<dbpassword>@ds052968.mlab.com:52968/udemy-todo-app
-var connectPath, options;
-//Check if we are on Heroku
-if(process.env.PORT){
- connectPath = process.env.MONGOLAB_URI; //"mongodb://udemy:tAAsjzdc@ds052968.mlab.com:52968/udemy-todo-app";
-
- options= {
-     auth: {
-         user: 'udemy',
-         password: 'tAAsjzdc'
-     }
- }
-}else{
- connectPath = "mongodb://localhost:27017/TodoApp";
- options = {}
-}
-mongoose.connect(connectPath, options);
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose};
