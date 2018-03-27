5dc6f5f8593dbb7396cf8ad90ea18659516500bf
Sybren van Wayenburg <sybren@vanwayenburg.net>
2018-03-27 21:45:40 +0200

    Hash user password before saving

--- server/server.js
+++ server/server.js
@@ -96,8 +96,6 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
-
-// POST /Users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
